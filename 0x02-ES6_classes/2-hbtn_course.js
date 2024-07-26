8196be9f898072b84a9990f868249fc8ba1b6c61
Addizz-cell <iwayemiolawale@gmail.com>
2024-07-26 07:12:00 -0400

    Classes made

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,13 +1,48 @@
-export default class HolbertonCourse {
-    constructor(name, length, students) {
-      if (typeof name !== 'string' || typeof length !== 'number' || !Array.isArray(students)) {
-        throw new Error('Invalid input type');
-      }
-      this._name = name;
-      this._length = length;
-      this._students = students;
+class HolbertonCourse {
+  constructor(name, length, students) {
+    // Verify types during object creation
+    if (typeof name !== 'string') throw new TypeError('Name must be a string');
+    if (typeof length !== 'number') throw new TypeError('Length must be a number');
+    if (!Array.isArray(students) || !students.every(student => typeof student === 'string')) {
+      throw new TypeError('Students must be an array of strings');
     }
-  
-    get name() {
-      return this._name;
+
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  // Getter and setter for name
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof newName !== 'string') throw new TypeError('Name must be a string');
+    this._name = newName;
+  }
+
+  // Getter and setter for length
+  get length() {
+    return this._length;
+  }
+
+  set length(newLength) {
+    if (typeof newLength !== 'number') throw new TypeError('Length must be a number');
+    this._length = newLength;
+  }
+
+  // Getter and setter for students
+  get students() {
+    return this._students;
+  }
+
+  set students(newStudents) {
+    if (!Array.isArray(newStudents) || !newStudents.every(student => typeof student === 'string')) {
+      throw new TypeError('Students must be an array of strings');
     }
+    this._students = newStudents;
+  }
+}
+
+export default HolbertonCourse;
